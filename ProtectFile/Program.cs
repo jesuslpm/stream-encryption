491276093fcf8d86b6b10562d708d07c42184da2
Jesús López <jesus.lopez@inercya.com>
2024-01-24 08:36:51 +0000

    add protect and unprotect file

---
+++ ProtectFile/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ProtectFile
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1) 
+            {
+                Console.Error.WriteLine("Usage: ProtectFile <file to protect>");
+                return 1;
+            }
+            var plainBytes = File.ReadAllBytes(args[0]);
+            var entropy = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(entropy);
+            }
+
+            var cipherBytes = ProtectedData.Protect(plainBytes, entropy, DataProtectionScope.CurrentUser);
+
+            using (var destFile = File.Create(args[0] + ".protected")) 
+            { 
+                destFile.Write(entropy, 0, entropy.Length);
+                destFile.Write(cipherBytes, 0, cipherBytes.Length);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ UnprotectFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace UnprotectFile
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: UnprotectFile <file to unprotect>");
+                return 1;
+            }
+            var sourcePath = args[0];
+            var extension = Path.GetExtension(sourcePath);
+            if (extension != ".protected")
+            {
+                Console.Error.WriteLine("protected files must have .protected extension");
+                return 1;
+            }
+            var destPath = sourcePath.Substring(0, sourcePath.Length - extension.Length);
+            var entropy = new byte[16];
+            var encryptedBytes = File.ReadAllBytes(sourcePath);
+            Array.Copy(encryptedBytes, 0, entropy, 0, 16);
+            var cipherBytes = new byte[encryptedBytes.Length - 16];
+            Array.Copy(encryptedBytes, 16, cipherBytes, 0, cipherBytes.Length);
+            var plainBytes = ProtectedData.Unprotect(cipherBytes, entropy, DataProtectionScope.CurrentUser);
+            File.WriteAllBytes(destPath, plainBytes);
+            return 0;
+        }
+    }
+}
